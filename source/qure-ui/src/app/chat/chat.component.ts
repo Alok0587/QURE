82a59d5c86fd12a6b7150e17f2f80f00df8fbfd3
Alok Kumar <aloku111@in.ibm.com>
2019-11-04 00:42:55 +0530

    UI enhanced

--- source/qure-ui/src/app/chat/chat.component.ts
+++ source/qure-ui/src/app/chat/chat.component.ts
@@ -4,12 +4,15 @@ import { Component } from '@angular/core';
 import { WebSocketAPI } from './WebSocketAPI';
 import * as $ from "jquery";
 
+
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent {
+  
+
   title = 'angular8-springboot-websocket';
 
   webSocketAPI: WebSocketAPI;
@@ -21,23 +24,27 @@ export class ChatComponent {
   parser: string[];
   
   ngOnInit() {
+
     this.webSocketAPI = new WebSocketAPI(new ChatComponent());
-  }
-
-  connect() {
     this.webSocketAPI._connect();
-    $("#connected").attr("disabled", true);
-    $("#disconnected").attr("disabled", false);
-    this.delay(1000);
-    document.getElementById("activity").innerHTML="<p style='color:green'><b>Available</p>";
+    document.getElementById("activity").innerHTML="<b><div class='fa-1x' style='color:green'><i class='fas fa-circle-notch fa-spin'>&nbsp;</i>Available </div>";
 
 
   }
+
+  // connect() {
+  //   this.webSocketAPI._connect();
+  //   $("#connected").attr("disabled", true);
+  //   $("#disconnected").attr("disabled", false);
+  //   this.delay(1000);
+  //   document.getElementById("activity").innerHTML="<p style='color:green'><b>Available</p>";
+  //   console.log("connection started after loading the html page");
+
+
+  // }
 
   disconnect() {
     this.webSocketAPI._disconnect();
-    $("#disconnected").attr("disabled", true);
-    $("#connected").attr("disabled", false);
     document.getElementById("activity").innerHTML="<p style='color:red'><b>Not Available</p>";
 
 
@@ -50,7 +57,7 @@ export class ChatComponent {
     this.convo = JSON.parse(message);
     console.log(message.class);
     this.msg = this.convo.slice(13, this.convo.length - 2);
-     $("#convo").append("<tr><td>"+"<button type= 'button' class='btn btn-success' style='float:left;'>" + "<h5  style='color:white;'>"  +this.msg + "</h5>" + "<span class='time-left text-muted'>"+this.date+"</span>"+"</button>"+"</tr></td>"); 
+     $("#convo").append("<tr><td>"+"<button type= 'button' class='btn' style='float:left; background-color:#566573 ' >" + "<h5  style='color:white;'>"  +this.msg + "</h5>" + "<span class='time-left text-muted'>"+this.date+"</span>"+"</button>"+"</tr></td>"); 
 
   }
   async delay(ms: number) {
